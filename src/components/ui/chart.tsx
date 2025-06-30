2c2afe3bb0b402247d8dbaf9dd3e42e831a9c7c1
Austin Arndt <amckibben86@gmail.com>
2025-06-30 23:26:18 +0000

    Add a double space between plot point labels and values

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -228,7 +228,7 @@ const ChartTooltipContent = React.forwardRef<
                     )}
                     <div
                       className={cn(
-                        "flex flex-1 justify-between leading-none",
+                        "flex flex-1 gap-2 leading-none",
                         nestLabel ? "items-end" : "items-center"
                       )}
                     >
