7307852ac57363710d1cba75d75db6cbef0eaf78
Yuri Hoyal <yurihoyal@conjuredcode.com>
2025-07-04 15:16:10 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,5 +1,3 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
 {pkgs}: {
   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
